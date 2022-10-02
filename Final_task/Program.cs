4f07f90a75360091ba990fb2178b175587fa9ba1
Andrey_Demidov <a_demidov91@mail.ru>
2022-10-02 21:06:39 +0300

    Добавили файл Readme

---
+++ Final_task/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] array = {"Hello", "2", "world", ":-)"};
+string[] NewArray = new string[array.Length];
+
+
+void NewArrayIf(string[] array, string[] NewArray)
+{
+    int size = 0;
+for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        NewArray[size] = array[i];
+        size++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+NewArrayIf(array, NewArray);
+PrintArray(NewArray);
